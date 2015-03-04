b55cded943745e4d7786fa6c593b00391a73bb08
Hunor Kovács <hunor.kovacs@ft.com>
2015-03-04 14:29:03 +0200

    Generating web URL only for articles.

--- api-policy-component-service/src/test/java/com/ft/up/apipolicy/filters/WebUrlCalculatorTest.java
+++ api-policy-component-service/src/test/java/com/ft/up/apipolicy/filters/WebUrlCalculatorTest.java
@@ -5,153 +5,172 @@ import com.ft.up.apipolicy.pipeline.HttpPipelineChain;
 import com.ft.up.apipolicy.pipeline.MutableRequest;
 import com.ft.up.apipolicy.pipeline.MutableResponse;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
-import org.apache.commons.lang.mutable.Mutable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.nio.charset.*;
 import java.util.Collections;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-/**
- * WebUrlCalculatorTest
- *
- * @author Simon.Gibbs
- */
 @RunWith(MockitoJUnitRunner.class)
 public class WebUrlCalculatorTest {
 
-    public final static String ERROR_RESPONSE = "{ \"message\" : \"Error\" }";
-
-    public final static String MINIMAL_EXAMPLE_IDENTIFIER_RESPONSE = "{ \"identifiers\": [{\n" +
+    private final static Charset UTF8 = Charset.forName("UTF-8");
+    private final static String ERROR_RESPONSE = "{ \"message\" : \"Error\" }";
+    private final static byte[] MINIMAL_EXAMPLE_NON_ARTICLE_IDENTIFIER_RESPONSE = ("{ \"identifiers\": [{\n" +
             "\"authority\": \"http://www.ft.com/ontology/origin/FT-CLAMO\",\n" +
             "\"identifierValue\": \"219512\"\n" +
-            "}] }";
-
-    public final static String MINIMAL_EXAMPLE_CONTENTORIGIN_RESPONSE = "{ \"contentOrigin\": {\n" +
+            "}] }").getBytes(UTF8);
+    private final static byte[] MINIMAL_EXAMPLE_ARTICLE_IDENTIFIER_RESPONSE = ("{ \"identifiers\": [{\n" +
+            "\"authority\": \"http://www.ft.com/ontology/origin/FT-CLAMO\",\n" +
+            "\"identifierValue\": \"219512\"\n" +
+            "}],\n" +
+            "\"type\": \"http://www.ft.com/ontology/content/Article\" }").getBytes(UTF8);
+    private final static byte[] MINIMAL_EXAMPLE_NON_ARTICLE_CONTENT_ORIGIN_RESPONSE = ("{ \"contentOrigin\": {\n" +
             "\"originatingSystem\": \"http://www.ft.com/ontology/origin/FT-CLAMO\",\n" +
             "\"originatingIdentifier\": \"581814c4-748e-11e4-b30b-00144feabdc0\"\n" +
-            "}}";
-
-    public final static String NO_IDENTIFIERS_RESPONSE =  "{ \"identifiers\": [] }";
-    public final static String NO_CONTENT_ORIGIN_RESPONSE =  "{ \"contentOrigin\": {} }";
-
-    public Map<String,String> FASTFT_TEMPLATE = Collections.singletonMap("http://www.ft.com/ontology/origin/FT-CLAMO","TEST{{originatingIdentifier}}");
+            "}}").getBytes(UTF8);
+    private final static byte[] MINIMAL_EXAMPLE_ARTICLE_CONTENT_ORIGIN_RESPONSE = ("{ \"contentOrigin\": {\n" +
+            "\"originatingSystem\": \"http://www.ft.com/ontology/origin/FT-CLAMO\",\n" +
+            "\"originatingIdentifier\": \"581814c4-748e-11e4-b30b-00144feabdc0\"\n" +
+            "},\n" +
+            "\"type\": \"http://www.ft.com/ontology/content/Article\"}").getBytes(UTF8);
+    private final static String NO_IDENTIFIERS_RESPONSE = "{ \"identifiers\": [] }";
+    private final static String NO_CONTENT_ORIGIN_RESPONSE = "{ \"contentOrigin\": {} }";
+    private final static Map<String, String> FASTFT_TEMPLATE =
+            Collections.singletonMap("http://www.ft.com/ontology/origin/FT-CLAMO", "TEST{{originatingIdentifier}}");
 
     @Mock
     private HttpPipelineChain mockChain;
 
-
     private MutableRequest exampleRequest = new MutableRequest(Collections.singleton("TEST"), getClass().getSimpleName());
 
     private MutableResponse exampleErrorResponse;
-    private MutableResponse minimalExampleResponse;
+    private MutableResponse minimalExampleIdentifierNonArticleResponse;
+    private MutableResponse minimalExampleIdentifierArticleResponse;
+    private MutableResponse minimalExampleContentOriginNonArticleResponse;
+    private MutableResponse minimalExampleContentOriginArticleResponse;
     private MutableResponse originatingSystemIsNullResponse;
-    private MutableResponse minimalContentOriginResponse;
     private MutableResponse contentOriginIsNullResponse;
 
     @Before
     public void setUpExamples() {
-        exampleErrorResponse = new MutableResponse(new MultivaluedMapImpl(),ERROR_RESPONSE.getBytes());
+        exampleErrorResponse = new MutableResponse(new MultivaluedMapImpl(), ERROR_RESPONSE.getBytes());
         exampleErrorResponse.setStatus(500);
 
-        minimalExampleResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_IDENTIFIER_RESPONSE.getBytes());
-        minimalExampleResponse.setStatus(200);
+        minimalExampleIdentifierNonArticleResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_NON_ARTICLE_IDENTIFIER_RESPONSE);
+        minimalExampleIdentifierNonArticleResponse.setStatus(200);
+        minimalExampleIdentifierNonArticleResponse.getHeaders().putSingle("Content-Type", "application/json");
+
+        minimalExampleContentOriginNonArticleResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_NON_ARTICLE_CONTENT_ORIGIN_RESPONSE);
+        minimalExampleContentOriginNonArticleResponse.setStatus(200);
+        minimalExampleContentOriginNonArticleResponse.getHeaders().putSingle("Content-Type", "application/json");
+
+        minimalExampleIdentifierArticleResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_ARTICLE_IDENTIFIER_RESPONSE);
+        minimalExampleIdentifierArticleResponse.setStatus(200);
+        minimalExampleIdentifierArticleResponse.getHeaders().putSingle("Content-Type", "application/json");
+
+        minimalExampleContentOriginArticleResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_ARTICLE_CONTENT_ORIGIN_RESPONSE);
+        minimalExampleContentOriginArticleResponse.setStatus(200);
+        minimalExampleContentOriginArticleResponse.getHeaders().putSingle("Content-Type", "application/json");
 
         originatingSystemIsNullResponse = new MutableResponse(new MultivaluedMapImpl(), NO_IDENTIFIERS_RESPONSE.getBytes());
         originatingSystemIsNullResponse.setStatus(200);
-
-        minimalContentOriginResponse = new MutableResponse(new MultivaluedMapImpl(), MINIMAL_EXAMPLE_CONTENTORIGIN_RESPONSE.getBytes());
-        minimalContentOriginResponse.setStatus(200);
+        originatingSystemIsNullResponse.getHeaders().putSingle("Content-Type", "application/json");
 
         contentOriginIsNullResponse = new MutableResponse(new MultivaluedMapImpl(), NO_CONTENT_ORIGIN_RESPONSE.getBytes());
         contentOriginIsNullResponse.setStatus(200);
-
-        minimalExampleResponse.getHeaders().putSingle("Content-Type","application/json");
-        originatingSystemIsNullResponse.getHeaders().putSingle("Content-Type","application/json");
-        minimalContentOriginResponse.getHeaders().putSingle("Content-Type","application/json");
-
-        contentOriginIsNullResponse.getHeaders().putSingle("Content-Type","application/json");
+        contentOriginIsNullResponse.getHeaders().putSingle("Content-Type", "application/json");
     }
 
     @Test
     public void shouldNotProcessErrorResponse() {
-
         when(mockChain.callNextFilter(exampleRequest)).thenReturn(exampleErrorResponse);
-
         WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
 
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
 
         assertThat(response.getEntity(), is(ERROR_RESPONSE.getBytes()));
-
     }
 
     @Test
     public void shouldNotProcessJSONLD() {
-
-        minimalExampleResponse.getHeaders().putSingle("Content-Type", "application/ld-json");
-        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleResponse);
-
+        minimalExampleIdentifierNonArticleResponse.getHeaders().putSingle("Content-Type", "application/ld-json");
+        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleIdentifierNonArticleResponse);
         WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
 
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
 
-        assertThat(response.getEntity(),is(MINIMAL_EXAMPLE_IDENTIFIER_RESPONSE.getBytes()));
+        assertThat(response.getEntity(), is(MINIMAL_EXAMPLE_NON_ARTICLE_IDENTIFIER_RESPONSE));
     }
 
     @Test
-    public void shouldAddWebUrlToSuccessResponse() {
-        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleResponse);
-
+    public void shouldNotAddWebUrlToSuccessResponseForNonArticlesWithIdentifiers() {
+        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleIdentifierNonArticleResponse);
         WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
 
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
 
-        assertThat(response.getEntityAsString(),containsString("\"webUrl\":\"TEST219512\""));
+        assertThat(response.getEntity(), is(MINIMAL_EXAMPLE_NON_ARTICLE_IDENTIFIER_RESPONSE));
+    }
+
+    @Test
+    public void shouldAddWebUrlToSuccessResponseForArticlesWithIdentifiers() {
+        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleIdentifierArticleResponse);
+        WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
+
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
+
+        assertThat(response.getEntityAsString(), containsString("\"webUrl\":\"TEST219512\""));
+    }
+
+    @Test
+    public void shouldNotAddWebUrlToSuccessResponseForNonArticlesWithContentOrigin() {
+        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleContentOriginNonArticleResponse);
+        WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
+
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
+
+        assertThat(response.getEntity(), is(MINIMAL_EXAMPLE_NON_ARTICLE_CONTENT_ORIGIN_RESPONSE));
+    }
+
+    @Test
+    public void shouldAddWebUrlToSuccessResponseForArticlesWithContentOrigin() {
+        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalExampleContentOriginArticleResponse);
+        WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
+
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
+
+        assertThat(response.getEntityAsString(), containsString("\"webUrl\":\"TEST581814c4-748e-11e4-b30b-00144feabdc0\""));
     }
 
     @Test
     public void shouldReturnSuccessResponseWithoutWebUrlWhenOriginatingSystemIsNull() {
         when(mockChain.callNextFilter(exampleRequest)).thenReturn(originatingSystemIsNullResponse);
-
         WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
 
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
 
-        assertThat(response.getEntityAsString(),not(containsString("\"webUrl\":")));
+        assertThat(response.getEntityAsString(), not(containsString("\"webUrl\":")));
 
     }
 
     @Test
-    public void shouldAddWebUrlToSuccessfulResponseWithContentOrigin(){
-        when(mockChain.callNextFilter(exampleRequest)).thenReturn(minimalContentOriginResponse);
-
-        WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
-
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
-
-        assertThat(response.getEntityAsString(),containsString("\"webUrl\":\"TEST581814c4-748e-11e4-b30b-00144feabdc0\""));
-    }
-
-    @Test
-    public void shouldReturnSuccessResponseWithoutWebUrlWhenContentOriginIsNull(){
+    public void shouldReturnSuccessResponseWithoutWebUrlWhenContentOriginIsNull() {
         when(mockChain.callNextFilter(exampleRequest)).thenReturn(contentOriginIsNullResponse);
-
         WebUrlCalculator calculator = new WebUrlCalculator(FASTFT_TEMPLATE, JsonConverter.testConverter());
 
-        MutableResponse response = calculator.processRequest(exampleRequest,mockChain);
+        MutableResponse response = calculator.processRequest(exampleRequest, mockChain);
 
-        assertThat(response.getEntityAsString(),not(containsString("\"webUrl\":")));
+        assertThat(response.getEntityAsString(), not(containsString("\"webUrl\":")));
     }
 }
