e3e988f1630db423634a06be3990ee8a2721dfa4
Yolande Leung <yolande.leung@ft.com>
2015-05-19 14:46:13 +0100

    added default route-by-path header value

--- api-policy-component-service/src/main/java/com/ft/up/apipolicy/JerseyRequestForwarder.java
+++ api-policy-component-service/src/main/java/com/ft/up/apipolicy/JerseyRequestForwarder.java
@@ -50,7 +50,8 @@ public class JerseyRequestForwarder implements RequestForwarder {
         MultivaluedMap<String,String> headers = request.getHeaders();
         for(String headerName : headers.keySet()) {
             if(headerName.equals("Host")){ // for Containerisation
-                LOGGER.info("Not forwarding 'Host' Header");
+                LOGGER.info("replacing 'Host' Header");
+                resource = resource.header(headerName, "route-by-path");
                 continue;
             }
             for(String value : headers.get(headerName)) {
