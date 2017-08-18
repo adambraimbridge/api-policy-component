9e8c84ebab647711523c816c6108c34a2b5f8615
Tamas Molnar <tamas.molnar@ft.com>
2017-08-18 09:56:02 +0300

    Added tests for node healthcheck path switch

--- api-policy-component-service/src/test/java/com/ft/up/apipolicy/health/ReaderNodesHealthCheckTest.java
+++ api-policy-component-service/src/test/java/com/ft/up/apipolicy/health/ReaderNodesHealthCheckTest.java
@@ -7,11 +7,16 @@ import com.sun.jersey.api.client.ClientHandler;
 import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.net.URI;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -20,6 +25,12 @@ import static org.mockito.Mockito.when;
  * @author Simon.Gibbs
  */
 public class ReaderNodesHealthCheckTest {
+
+    // used in stacks with vulcan-based routing
+    private static final String VULCAN_HC_PATH = "/v2/status";
+
+    //used in stacks with varnish-based routing
+    private static final String VARNISH_HC_PATH = "/status";
 
     @Test
     public void shouldReturnErrorStateIfClientThrowsException() throws Exception {
@@ -57,6 +68,32 @@ public class ReaderNodesHealthCheckTest {
         ReaderNodesHealthCheck check = new ReaderNodesHealthCheck("test",config,client,false);
 
         assertThat(check.checkAdvanced().status(),is(AdvancedResult.Status.OK));
+    }
+
+    @Test
+    public void shouldCheckVarnishIfCheckVulcanHealthFalse() throws Exception {
+        testHealthcheckURI(false, VARNISH_HC_PATH);
+    }
+
+    @Test
+    public void shouldCheckVulcanIfCheckVulcanHealthTrue() throws Exception {
+        testHealthcheckURI(true, VULCAN_HC_PATH);
+    }
+
+    private void testHealthcheckURI(boolean checkVulcanHealth, String expectedPath) throws Exception {
+        EndpointConfiguration config = mock(EndpointConfiguration.class);
+        Client client = mock(Client.class);
+
+        ReaderNodesHealthCheck check = new ReaderNodesHealthCheck("test", config, client, checkVulcanHealth);
+        check.checkAdvanced();
+
+        ArgumentCaptor<URI> argument = ArgumentCaptor.forClass(URI.class);
+        verify(client).resource(argument.capture());
+
+        assertNotNull(argument.getValue());
+        String path = argument.getValue().getPath();
+        assertNotNull(path);
+        assertThat(path, is(expectedPath));
     }
 
     private Client primeClientToGive(int status) {
