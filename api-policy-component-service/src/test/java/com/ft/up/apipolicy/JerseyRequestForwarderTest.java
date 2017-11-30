b2da8cbf8e4df11fc4665dc9058f5edbe9043e56
peteclark-io <pete.david.clark@gmail.com>
2017-11-30 13:33:38 +0000

    :ok_hand: Fix static imports and allow nulls body in MutableRequest

--- api-policy-component-service/src/test/java/com/ft/up/apipolicy/JerseyRequestForwarderTest.java
+++ api-policy-component-service/src/test/java/com/ft/up/apipolicy/JerseyRequestForwarderTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ft.up.apipolicy;
 
 import com.ft.up.apipolicy.configuration.EndpointConfiguration;
@@ -24,8 +19,10 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.anyString;
 
 /**
  *
@@ -33,19 +30,19 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class JerseyRequestForwarderTest {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(JerseyRequestForwarderTest.class);
-    
+
     private static final String FASTFT_BRAND = "http://api.ft.com/things/5c7592a8-1f0c-11e4-b0cb-b2227cce2b54";
-    
+
     @Mock private Client client;
     @Mock private WebTarget target;
     @Mock private Invocation.Builder builder;
     @Mock private Invocation invocation;
     @Mock private Response response;
-    
+
     @Mock private EndpointConfiguration endpointConfiguration;
-    
+
     @Before
     public void before(){
         when(endpointConfiguration.getHost()).thenReturn("hostname");
@@ -55,16 +52,15 @@ public class JerseyRequestForwarderTest {
         when(builder.build("GET")).thenReturn(invocation);
         when(invocation.invoke()).thenReturn(response);
     }
-    
+
     @Test
     public void testForwardConceptSearchRequest() {
         JerseyRequestForwarder forwarder = new JerseyRequestForwarder(client, endpointConfiguration);
-        
+
         MutableRequest request = new MutableRequest(Sets.newHashSet(), "tid_1234");
         request.setAbsolutePath("/concepts");
         request.setHttpMethod("GET");
-        request.setRequestEntity("".getBytes());
-        
+
         MultivaluedMap queryParams = new MultivaluedHashMap();
         queryParams.add("mode", "search");
         queryParams.add("type", "http://www.ft.com/ontology/person/Person");
@@ -72,59 +68,58 @@ public class JerseyRequestForwarderTest {
         queryParams.add("type", "http://www.ft.com/ontology/Location");
         queryParams.add("type", "http://www.ft.com/ontology/Topic");
         queryParams.add("q", "new york");
-        
+
         request.setQueryParameters(queryParams);
-        
+
         MultivaluedMap headers = new MultivaluedHashMap();
         headers.add("X-Test-Header", "EXAMPLE");
         request.setHeaders(headers);
-        
+
         URI conceptSearch = URI.create("http://hostname:8080/concepts?mode=search&q=new+york&type=http%3A%2F%2Fwww.ft.com%2Fontology%2Fperson%2FPerson&type=http%3A%2F%2Fwww.ft.com%2Fontology%2Forganisation%2FOrganisation&type=http%3A%2F%2Fwww.ft.com%2Fontology%2FLocation&type=http%3A%2F%2Fwww.ft.com%2Fontology%2FTopic");
-        
+
         when(client.target(conceptSearch)).thenReturn(target);
         when(response.getHeaders()).thenReturn(new MultivaluedHashMap());
         when(response.getStatus()).thenReturn(200);
-        
+
         MutableResponse actualResponse = forwarder.forwardRequest(request);
         assertEquals(HttpServletResponse.SC_OK, actualResponse.getStatus());
-        
+
         verify(client).target(conceptSearch);
         verifyMocks();
     }
-    
+
     @Test
-    public void testForwardNotificationsForConceptRequest() {
+    public void testForwardNotificationsRequest() {
         JerseyRequestForwarder forwarder = new JerseyRequestForwarder(client, endpointConfiguration);
-        
+
         MutableRequest request = new MutableRequest(Sets.newHashSet(), "tid_1234");
         request.setAbsolutePath("/content/notifications");
         request.setHttpMethod("GET");
-        request.setRequestEntity("".getBytes());
-        
+
         MultivaluedMap queryParams = new MultivaluedHashMap();
         queryParams.add("forBrand", FASTFT_BRAND);
         queryParams.add("notForBrand", FASTFT_BRAND);
         queryParams.add("since", "2017-10-17T15:22:49.804Z");
-        
+
         request.setQueryParameters(queryParams);
-        
+
         MultivaluedMap headers = new MultivaluedHashMap();
         headers.add("X-Test-Header", "EXAMPLE");
         request.setHeaders(headers);
-        
+
         URI notificationsForBrand = URI.create("http://hostname:8080/content/notifications?forBrand=http%3A%2F%2Fapi.ft.com%2Fthings%2F5c7592a8-1f0c-11e4-b0cb-b2227cce2b54&notForBrand=http%3A%2F%2Fapi.ft.com%2Fthings%2F5c7592a8-1f0c-11e4-b0cb-b2227cce2b54&since=2017-10-17T15%3A22%3A49.804Z");
-                
+
         when(client.target(notificationsForBrand)).thenReturn(target);
         when(response.getHeaders()).thenReturn(new MultivaluedHashMap());
         when(response.getStatus()).thenReturn(200);
-        
+
         MutableResponse actualResponse = forwarder.forwardRequest(request);
         assertEquals(HttpServletResponse.SC_OK, actualResponse.getStatus());
-        
+
         verify(client).target(notificationsForBrand);
         verifyMocks();
     }
-    
+
     private void verifyMocks(){
         verify(response).getHeaders();
         verify(response).getStatus();
